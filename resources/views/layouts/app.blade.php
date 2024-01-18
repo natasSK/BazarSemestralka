cb2c3f9f0d4d006aea963cebe7387d1676bfc550
Patrik <petrakpatrik@gmail.com>
2024-01-18 13:22:02 +0100

    Pridaný odkaz moje inzeráty

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,10 +55,13 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="/profile/{{Auth::user()->id}}">
+                                        {{ __('Moje inzeráty') }}
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                        {{ __('Odhlásiť sa') }}
                                     </a>
 
 
