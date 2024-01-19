32f19f7ad6f9f7546141298a57fa086e2509f1f4
Patrik <petrakpatrik@gmail.com>
2024-01-19 20:11:08 +0100

    Uprava detailu

--- resources/views/advert.blade.php
+++ resources/views/advert.blade.php
@@ -5,13 +5,31 @@
         <div class="row" style="height: 400px;">
             <div class="col-lg-5">
                 <div class="container">
-                    <img class="image-fluid" src="https://www.pacificfoodmachinery.com.au/media/catalog/product/placeholder/default/no-product-image-400x400.png" alt="Second slide" style="width: 400px; height: 400px">
+                    <div class="d-flex">
+                        <div class="position-relative">
+                            <img class="image-fluid" src="https://www.pacificfoodmachinery.com.au/media/catalog/product/placeholder/default/no-product-image-400x400.png" alt="Second slide" style="width: 400px; height: 400px">
+
+                            @can('update', $advert)
+                                <div class="position-absolute top-0 start-0">
+                                    <a href="/adverts/{{ $advert->id }}/edit" class="btn btn-warning btn-lg mt-1 mx-1">Uprav</a>
+                                    <form method="POST" action="/adverts/{{ $advert->id }}/delete">
+                                        @csrf
+                                        @method('delete')
+                                        <button class="btn btn-danger btn-lg mt-1 mx-1">Zmaž</button>
+                                    </form>
+                                </div>
+                            @endcan
+                        </div>
+                    </div>
                 </div>
+
+
             </div>
             <div class="col-lg textInfo mx-3">
                 <div class="row my-2 mx-1 text-truncate fw-bold fs-1" style="height: 15%">
                     <p>{{ $advert->title }}</p>
                 </div>
+
                 <div class="row">
                     <div class="col-7 py-1">
                         <a href="{{ route('profile.show', ['user' => $advert->user_id]) }}" style="text-decoration: none; color: black">
@@ -36,48 +54,16 @@
                     </div>
 
                 </div>
-
-
-                <div class="row my-2 mx-1 overflow-scroll overflow-x-hidden overflow-y-auto border-light-subtle" style="height: 45%; padding: 10px; border: 1px solid">
-                    Základné parametre:
-
-                    OS: Android 13 + MIUI 14
-
-                    čipset: Qualcomm Snapdragon 8 Gen 2, 4 nm
-
-                    procesor: osemjadrový, 1 x 3,2 GHz + 2 x 2,8 GHz + 2 x 2,8 GHz + 3 x 2,0 GHz
-
-                    grafický procesor: Adreno 740
-
-                    displej: 6,73'' (171 mm), LTPO OLED, 1440 x 3200 b., 1 - 120 Hz, HDR10+, 522 ppi, Gorilla Glass Victus, jas až 2600 nitov
-
-                    zadný fotoaparát (23 mm): 50 Mpix, IMX989, f/1.9, f/4.0, video 8K (24 fps) / 4K (60 fps), AF, OIS
-
-                    širokouhlý fotoaparát (12 mm): 50 Mpix, IMX858m f/1.8, 122°, AF, makro od 5 cm
-
-                    teleobjektív (75 mm): 50 Mpix, IMX858, f/1.8, PDAF, OIS
-
-                    periskopický teleobjektív (120 mm): 50 Mpix, IMX858, f/3.0, 5x optický zoom, PDAF, OIS
-
-                    predný fotoaparát: 32 Mpix, f/2.0, video Full HD (30 fps)
+                <div class="row my-2 mx-1">
+                    Stav: nové, Typ: predaj, z dňa {{ $advert->created_at }} (č. {{ $advert->id }})
                 </div>
-            </div>
-            @can('update', $advert)
-            <div class="container mt-1">
-                <div class="row">
-                    <div class="col-xxl-1 col-xl-10 mb-1">
-                        <a href="/adverts/{{ $advert->id }}/edit" class="btn btn-warning btn-lg btn-block">Uprav</a>
-                    </div>
-                    <div class="col-xxl-1 col-xl-4">
-                        <form method="POST" action="/adverts/{{ $advert->id }}/delete">
-                            @csrf
-                            @method('delete')
-                            <button class="btn btn-danger btn-lg btn-block">Zmaž</button>
-                        </form>
-                    </div>
+
+                <div class="row my-2 mx-1 overflow-scroll overflow-x-hidden overflow-y-auto border-light-subtle" style="height: 28%; padding: 10px; border: 1px solid">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ullamcorper ullamcorper blandit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec elementum risus libero, ac blandit est ultrices vulputate. Phasellus vitae porta felis. Curabitur eget massa libero. Aliquam maximus, libero sit amet ornare dignissim, quam quam rhoncus tellus, vitae pharetra ante magna lacinia enim. Nulla facilisi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce malesuada at massa non semper. Donec vel odio ut augue dictum lobortis in quis nibh. Nunc sit amet massa massa. Morbi consequat nunc a libero pharetra, eget efficitur metus lobortis. Vivamus mauris ligula, malesuada vel arcu ac, lobortis iaculis justo. Nam pharetra, nunc id efficitur viverra, velit magna pulvinar quam, in iaculis est libero ac magna. Vestibulum et diam quis nulla vestibulum sagittis et ac lorem. In hac habitasse platea dictumst.
                 </div>
+
             </div>
-            @endcan
+
 
         </div>
 
